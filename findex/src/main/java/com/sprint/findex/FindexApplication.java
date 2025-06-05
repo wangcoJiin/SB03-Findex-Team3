336dea6bdaa52217eeea8ff1c1122381bb9d16de
wangcoJiin <mf0d070208@gmail.com>
2025-06-05 11:21:35 +0900

    init: project 초기화

---
+++ findex/src/main/java/com/sprint/findex/FindexApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.findex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindexApplication.class, args);
+    }
+
+}
